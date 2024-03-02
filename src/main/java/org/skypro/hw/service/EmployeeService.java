2425063aa4a86792bb17903e9daeb2820421453f
makkov <maxim_86nva@mail.ru>
2024-03-02 11:05:41 +0300

    подключена библиотека apache-common-langs3, реализована валидация входных данных

--- src/main/java/org/skypro/hw/service/EmployeeService.java
+++ src/main/java/org/skypro/hw/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package org.skypro.hw.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.skypro.hw.entity.Employee;
 import org.skypro.hw.exception.EmployeeAlreadyAddedException;
 import org.skypro.hw.exception.EmployeeNotFoundException;
 import org.skypro.hw.exception.EmployeeStorageIsFullException;
+import org.skypro.hw.exception.ValidationException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,6 +19,8 @@ public class EmployeeService {
     private final static int MAX_SIZE = 2;
 
     public Employee add(String firstName, String lastName, double salary, int departmentId) {
+        validateCapitalize(firstName, lastName);
+        validateIsAlpha(firstName, lastName);
 
         if (employees.size() >= MAX_SIZE) {
             throw new EmployeeStorageIsFullException("Массив сотрудников переполнен");
@@ -33,6 +37,9 @@ public class EmployeeService {
     }
 
     public Employee find(String firstName, String lastName, double salary, int departmentId) {
+        validateCapitalize(firstName, lastName);
+        validateIsAlpha(firstName, lastName);
+
         Employee employeeForFound = new Employee(firstName, lastName, salary, departmentId);
         for (Employee e : employees) {
             if (e.equals(employeeForFound)) {
@@ -44,6 +51,9 @@ public class EmployeeService {
     }
 
     public Employee remove(String firstName, String lastName, double salary, int departmentId) {
+        validateCapitalize(firstName, lastName);
+        validateIsAlpha(firstName, lastName);
+
         Employee employeeForRemove = new Employee(firstName, lastName, salary, departmentId);
 
         boolean removeResult = employees.remove(employeeForRemove);
@@ -56,5 +66,27 @@ public class EmployeeService {
 
     public List<Employee> getAll() {
         return employees;
+    }
+
+    private void validateCapitalize(String firstName, String lastName) {
+        String capitalizeFirstName = StringUtils.capitalize(firstName);
+        if (!firstName.equals(capitalizeFirstName)) {
+            throw new ValidationException("Имя сотрудника начинается не с большой буквы");
+        }
+
+        String capitalizeLastName = StringUtils.capitalize(lastName);
+        if (!lastName.equals(capitalizeLastName)) {
+            throw new ValidationException("Фамилия сотрудника начинается не с большой буквы");
+        }
+    }
+
+    private void validateIsAlpha(String firstName, String lastName) {
+        if (!StringUtils.isAlpha(firstName)) {
+            throw new ValidationException("Имя содержит запрещенные символы");
+        }
+
+        if (!StringUtils.isAlpha(lastName)) {
+            throw new ValidationException("Фамилия содержит запрещенные символы");
+        }
     }
 }
